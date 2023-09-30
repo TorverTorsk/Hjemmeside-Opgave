a92cb58752b6f6711e402590e3484b74acb0f06f
TorveTorsk <a.n.terkelsen@gmail.com>
2023-09-30 12:33:44 +0200

    www added copyright and almost ønsker

---
+++ countdown.php
@@ -0,0 +1,12 @@
+<?php 
+include_once "navbar.html"
+?>
+
+
+	<div id="count1">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til Efterårsferien</div>
+	<div id="count2">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til Juleferien </div>
+	<div id="count3">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til min dimmisonsdag </div>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+include_once "navbar.html"
+?>
+
+	<table class="center">
+		<tr><td >
+			<div>
+				<div style="text-align: center; padding: 10px 20px;" id="1" class="center">Velkommen til min hjemmeside. Her kan du se meget vigtig information.</div>
+				<img src="images/hc_oersted.png" class="Hcoe">
+			</div>
+		</td></tr>
+	</table>
+
+</body>
+</html>
