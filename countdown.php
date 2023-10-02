32c5b4ddcd30f3fac6db8bf7fc5055e7c984f0da
TorveTorsk <a.n.terkelsen@gmail.com>
2023-10-02 10:19:21 +0200

    Ønsker færdigt og andet
    images folder secured, countdown lidt bedre

--- countdown.php
+++ countdown.php
@@ -2,10 +2,12 @@
 include_once "navbar.html"
 ?>
 
-
-	<div id="count1">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til Efterårsferien</div>
-	<div id="count2">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til Juleferien </div>
-	<div id="count3">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til min dimmisonsdag </div>
+<center>
+	<h3 id="count1">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til Efterårsferien</h3>
+	<h3 id="count2">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til Juleferien </h3>
+	<h3 id="count3">Der er 0 måneder, 0 dage , 0 timer, 0 minutter og 0 sekunder til min dimmisonsdag </h3>
+</center>
+	
 
 
 </body>
--- oensker.php
+++ oensker.php
@@ -2,7 +2,15 @@
 include_once "navbar.html"
 ?>
 <center>
-<h3></h3>
+    <h2 style="margin-bottom: 40px;" >Ønsker til turen til prag:</h2>
+    
+    <h3>Morgenmad alle dage </h3>
+    <h3>Technické muzeum</h3>
+    <h3>Amazon</h3>
+    <h3>Besøge startup firmaer</h3>
+    <h3>Deltage i hackation</h3>
+    <h3>Prags fantastiske Tekniske Universitet</h3>
+
 
 </center>
 
